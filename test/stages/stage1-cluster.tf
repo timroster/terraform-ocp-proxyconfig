46588e259e6ae0651d3c3f76c0803838c1361352
Tim Robinson <timroster@gmail.com>
2022-03-29 07:57:15 -0700

    update test to create cluster
    Signed-off-by: Tim Robinson <timroster@gmail.com>

--- test/stages/stage1-cluster.tf
+++ test/stages/stage1-cluster.tf
@@ -5,13 +5,13 @@ module "dev_cluster" {
   region              = var.region
   ibmcloud_api_key    = var.ibmcloud_api_key
   name                = var.cluster_name
-  worker_count        = 0
-  ocp_version         = "4.8"
-  exists              = var.cluster_exists
-  name_prefix         = var.name_prefix
-  vpc_name            = var.vpc_cluster
-  vpc_subnets         = []
-  vpc_subnet_count    = 0
-  cos_id              = ""
+  worker_count        = var.worker_count
+  ocp_version         = var.ocp_version
+  exists              = "false"
+  name_prefix         = local.name_prefix_test
+  vpc_name            = module.subnets.vpc_name
+  vpc_subnets         = module.subnets.subnets
+  vpc_subnet_count    = module.subnets.count
+  cos_id              = module.cos.id
   login               = "true"
 }
