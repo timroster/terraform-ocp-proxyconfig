83d9264568709dba74b67ede5ce97e1b7a8ee7de
Tim Robinson <timroster@gmail.com>
2022-03-29 11:18:59 -0700

    vary name of ssh key
    Signed-off-by: Tim Robinson <timroster@gmail.com>

--- test/stages/stage1-vpc-ssh.tf
+++ test/stages/stage1-vpc-ssh.tf
@@ -2,7 +2,7 @@ module "vpcssh" {
   source = "github.com/cloud-native-toolkit/terraform-ibm-vpc-ssh.git"
 
   resource_group_name = module.resource_group.name
-  name_prefix         = var.name_prefix
+  name_prefix         = local.name_prefix_test
   public_key          = ""
   private_key         = ""
 }
